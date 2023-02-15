6d1839e48498acd334c3e55f8887bf0f65b00674
Ajin <ajinmla94@gmail.com>
2023-02-15 22:37:45 +0530

    initial commit

---
+++ index/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import contactform
+
+def home(request):
+    return render(request, 'index.html' )
+
+def contacts(request):
+    if request .method == 'POST' :
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        contactformdata = contactform(name = name, email = email, subject = subject, message = message)
+        contactformdata.save()
+    return render(request, 'contacts.html')
+       
