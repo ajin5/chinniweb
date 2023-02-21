fca21e3f1dcde5cc41aeb363e90fd7475fb2da65
ajin5 <79011441+ajin5@users.noreply.github.com>
2023-02-21 10:20:22 +0530

    url update

--- chinninambi/settings.py
+++ chinninambi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#)$f3wo0dn_4p45o4ceq9e5e+_e-htif!ocy-#9&o*v%ze+zh(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://chinniweb-production.up.railway.app']
 
 
 # Application definition
@@ -129,3 +129,4 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'assets'
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
